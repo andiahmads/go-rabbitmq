1fc79ba2eb4dcd3a055d471cd63efbf03042c56b
andiahmads <andi.fivesco@gmail.com>
2022-02-10 15:27:38 +0700

    add loop infiniti

--- pubshub/emit_log.go
+++ pubshub/emit_log.go
@@ -10,37 +10,39 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("write your message =")
+	for true {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("write your message =")
 
-	mPayload, _ := reader.ReadString('\n')
+		mPayload, _ := reader.ReadString('\n')
 
-	// conn, err := amqp091.Dial("amqp://guest:guest@localhost:15672")
-	conn, err := amqp091.Dial("amqp://rabbitAdmin:B4yAr1nD123@34.101.130.110:5672/")
+		// conn, err := amqp091.Dial("amqp://guest:guest@localhost:15672")
+		conn, err := amqp091.Dial("amqp://rabbitAdmin:B4yAr1nD123@34.101.130.110:5672/")
 
-	if err != nil {
-		panic(errors.Wrap(err, "failed to connect to rabbitmq server"))
+		if err != nil {
+			panic(errors.Wrap(err, "failed to connect to rabbitmq server"))
+		}
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		if err != nil {
+			panic(errors.Wrap(err, "failed to get channel"))
+		}
+		defer ch.Close()
+		// q, err := ch.QueueDeclare("", true, false, true, false, nil)
+
+		// if err != nil {
+		// 	panic(errors.Wrap(err, "failed to declare queue"))
+		// }
+		err = ch.ExchangeDeclare("logs", amqp091.ExchangeFanout, true, false, false, false, nil)
+		if err != nil {
+			panic(errors.Wrap(err, "failed to declare exchange"))
+		}
+
+		err = ch.Publish("logs", "", false, false, amqp091.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(mPayload),
+		})
 	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		panic(errors.Wrap(err, "failed to get channel"))
-	}
-	defer ch.Close()
-	// q, err := ch.QueueDeclare("", true, false, true, false, nil)
-
-	// if err != nil {
-	// 	panic(errors.Wrap(err, "failed to declare queue"))
-	// }
-	err = ch.ExchangeDeclare("logs", amqp091.ExchangeFanout, true, false, false, false, nil)
-	if err != nil {
-		panic(errors.Wrap(err, "failed to declare exchange"))
-	}
-
-	err = ch.Publish("logs", "", false, false, amqp091.Publishing{
-		ContentType: "text/plain",
-		Body:        []byte(mPayload),
-	})
 
 }
